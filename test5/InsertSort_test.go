29c2bed3d54e1f01e7007482e774ab7eab6b9196
wobusbz <1135455278@qq.com>
2020-01-05 14:55:41 +0800

    插入排序

--- test5/InsertSort_test.go
+++ test5/InsertSort_test.go
@@ -3,7 +3,7 @@ package test5
 import "testing"
 
 func TestInsertSort(t *testing.T) {
-	intArr := []int{10, 2, 5, 7, 6, 100, 99}
+	intArr := []int{10, 2, 5, 7, 6, 100, 99, 33}
 	InsertSort(intArr)
 	intArr1 := []int{1, 2, 3, 4, 5, 6}
 	InsertSort1(intArr1)
