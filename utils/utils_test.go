b4b4f303e379f6c0b52f7adca793ebce2c508a50
wobusbz <1135455278@qq.com>
2020-01-05 13:40:44 +0800

    13点40分

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,12 +1,13 @@
 package utils
 
 import (
+	"arithmetic/test2"
 	"testing"
 )
 
 func TestCalcRunTime(t *testing.T) {
-	//intArr := RandList(100000)
-	//CalcRunTime("BubbleSort", test2.BubbleSort, intArr)
+	intArr := RandList(100000)
+	CalcRunTime1("BubbleSort", test2.BubbleSort, intArr)
 }
 
 func TestRandList(t *testing.T) {
