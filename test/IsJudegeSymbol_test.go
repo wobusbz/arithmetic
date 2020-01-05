40c0c875ac0c9716262ea6a74c3c544b3847490c
wobusbz <1135455278@qq.com>
2020-01-05 14:51:35 +0800

    找出字符串当中有效的()

--- test/IsJudegeSymbol_test.go
+++ test/IsJudegeSymbol_test.go
@@ -3,6 +3,6 @@ package test
 import "testing"
 
 func TestIsJudegeSymbol(t *testing.T) {
-	symbol := "asd(asdasd)asd)asasda(asdasd)(()123()asd(asd((asd(asd()("
+	symbol := "asd(asdasd)asd)asasda(asdasd)(()123()asd(asd((asd(sd()("
 	IsJudegeSymbol(symbol)
 }
