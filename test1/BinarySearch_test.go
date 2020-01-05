f1162b6609714504ae87d19a4a1f577aad9b8592
wobusbz <1135455278@qq.com>
2020-01-05 14:53:02 +0800

    二分查找

--- test1/BinarySearch_test.go
+++ test1/BinarySearch_test.go
@@ -3,6 +3,6 @@ package test1
 import "testing"
 
 func TestBinarySearch(t *testing.T) {
-	intArr := []int{10, 2, 5, 7, 6, 100, 99}
+	intArr := []int{10, 2, 5, 7, 6, 100, 99, 33}
 	BinarySearch(intArr, 50)
 }
