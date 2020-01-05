7d2efd7b898fc2af6fb4978d5ea4cff8c90f5ec4
wobusbz <1135455278@qq.com>
2020-01-05 14:54:02 +0800

    选择排序

--- test3/SelectSort_test.go
+++ test3/SelectSort_test.go
@@ -3,7 +3,7 @@ package test3
 import "testing"
 
 func TestSelectSort(t *testing.T) {
-	intArr := []int{10, 2, 5, 7, 6, 100, 99}
+	intArr := []int{10, 2, 5, 7, 6, 100, 99, 33}
 	SelectSort(intArr)
 	t.Log(intArr)
 }
