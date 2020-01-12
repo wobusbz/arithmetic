276dc160a1afe2a7b54f769bcacef1104872d215
wobusbz <1135455278@qq.com>
2020-01-13 00:11:21 +0800

    栈

--- test9/Stack.go
+++ test9/Stack.go
@@ -1,1 +1,33 @@
 package test9
+
+import "fmt"
+
+type Statck struct {
+	stack []int
+}
+
+func NewStatck() *Statck {
+	return &Statck{}
+}
+
+func (s *Statck) push(n int) {
+	s.stack = append(s.stack, n)
+}
+
+func (s *Statck) pop() int {
+	if s.len() == 0 {
+		return 0
+	}
+	value := s.stack[s.len()-1:][0]
+	s.stack = s.stack[:s.len()-1]
+	return value
+}
+
+// 返回栈得长度
+func (s *Statck) len() int {
+	return len(s.stack)
+}
+
+func (s *Statck) print() {
+	fmt.Println(s.stack)
+}
