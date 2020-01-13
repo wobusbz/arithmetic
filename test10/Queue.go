8a1e92447e5010a07ddb223093b2c9e102f394a1
wobusbz <1135455278@qq.com>
2020-01-13 23:54:47 +0800

    队列

---
+++ test10/Queue.go
@@ -0,0 +1,44 @@
+package test10
+
+type Queue struct {
+	queue []int
+	rear  int // 尾部
+	front int // 首部
+	size  int
+}
+
+const (
+	ERROREMPTY = 0
+)
+
+func (this *Queue) Init(size int) {
+	this.queue = make([]int, size)
+	this.rear = 0
+	this.front = 0
+	this.size = size
+}
+
+func (this *Queue) Push(element int) {
+	if !this.is_filled() {
+		this.rear = (this.rear + 1) % this.size
+		this.queue[this.rear] = element
+	}
+}
+
+func (this *Queue) Pop() int {
+	if !this.is_Empty() {
+		this.front = (this.front + 1) % this.size
+		return this.queue[this.front]
+	} else {
+		return ERROREMPTY
+	}
+
+}
+
+func (this *Queue) is_Empty() bool {
+	return this.rear == this.front
+}
+
+func (this *Queue) is_filled() bool {
+	return (this.rear+1)%this.size == this.front
+}
---
+++ test10/Queue_test.go
@@ -0,0 +1,14 @@
+package test10
+
+import "testing"
+
+func TestQueue_Init(t *testing.T) {
+	q := new(Queue)
+	q.Init(5)
+	for i := 2; i < 5; i++ {
+		q.Push(i)
+	}
+	t.Log(q.Pop())
+	t.Log(q.Pop())
+	t.Log(q.Pop())
+}
