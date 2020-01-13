e48cdce74ecfce85ef32ec475ca1a191cb616ab1
wobusbz <1135455278@qq.com>
2020-01-14 01:03:03 +0800

    修改单元测试

--- LeetCode/26/RemoveArr_test.go
+++ LeetCode/26/RemoveArr_test.go
@@ -9,11 +9,11 @@ var arrRemoveTest = []struct {
 	f     func([]int) int
 	out   int
 }{
-	{"RemoveArr", RemoveArr, 2},
+	{"RemoveArr", RemoveArr, 4},
 }
 
 func TestRemoveArr(t *testing.T) {
-	nums := []int{1, 1, 2, 2}
+	nums := []int{1, 2, 3, 3, 4, 4}
 	for _, v := range arrRemoveTest {
 		if v.f(nums) != v.out {
 			t.Errorf("RemoveArr:%d actual:%d", v.f(nums), v.out)
