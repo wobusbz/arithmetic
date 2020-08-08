6ab52129ba39f6aa023b4ed906673896836da7da
wuhuarou <wobusbz2019@qq.com>
2020-08-08 12:59:44 +0800

    add deal stock tow

--- dealStock/dealStock.go
+++ dealStock/dealStock.go
@@ -1,7 +1,11 @@
 package dealStock
 
+import (
+	"fmt"
+)
+
 func DealStock(prices []int32) int32 {
-	if len(prices) == 0 {
+	if len(prices) < 2 {
 		return 0
 	}
 	var maxProfit int32
@@ -12,6 +16,23 @@ func DealStock(prices []int32) int32 {
 		}
 	}
 	return maxProfit
+}
+
+func DealStock1(prices []int32) int32 {
+	if len(prices) < 2 {
+		return 0
+	}
+
+	var maxPrices = make([][2]int32, len(prices))
+	maxPrices[0][0] = 0
+	maxPrices[0][1] = -prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		maxPrices[i][0] = Max(maxPrices[i-1][0], maxPrices[i-1][1]+prices[i])
+		maxPrices[i][1] = Max(maxPrices[i-1][0]-prices[i], maxPrices[i-1][1])
+	}
+	fmt.Println(maxPrices)
+	return maxPrices[len(prices)-1][0]
 }
 
 func Max(min, max int32) int32 {
--- dealStock/dealStock_test.go
+++ dealStock/dealStock_test.go
@@ -9,4 +9,5 @@ import (
 func TestDealStock(t *testing.T) {
 	prices := []int32{7, 2, 1, 8, 3, 9}
 	assert.Equal(t, DealStock(prices), int32(13), "they should equal")
+	assert.Equal(t, DealStock1(prices), int32(13), "they should equal")
 }
